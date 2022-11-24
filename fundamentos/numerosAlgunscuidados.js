40c4ae87f2d4c23175670b0df7c8bc1d7de3e89c
Lila <dalila.analistadesistema@gmail.com>
2022-11-24 09:54:33 -0300

    numeros

---
+++ fundamentos/numerosAlgunscuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) //js tem um tipo chamado Infinity
+console.log("10" / 2) //Ele pega o valor e divide com tipo string
+console.log("show!" * 2) //Em JS não ai funcionar, vai dar NAN
+console.log(0.1 + 0.7)
+//console.log(10.toString())
+console.log((10.345).toFixed(2)) // consegue chamar o literal entre ()
