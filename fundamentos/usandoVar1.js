a45de83db3b024d99d52bc6e1a8bcaed75aa023a
Lila <dalila.analistadesistema@gmail.com>
2022-11-25 17:35:20 -0300

    usandoVar1

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+  {
+     {
+       {
+         var sera = 'Será???'
+       }
+     }
+  }
+}
+console.log(sera)
+
+function teste(){
+  var local = 123 //qdo define uma variavel dentro do corpo da função, o escolpo de uma function tem que ser dentro dela
+  console.log(local)
+}
+
+teste()
+console.log(local)
+
+//evitar variavel de escolpo global
