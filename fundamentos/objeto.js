4a6e35bf2bec77c65e5ea153f463bc1319e6c9a4
Lila <dalila.analistadesistema@gmail.com>
2022-11-25 11:11:01 -0300

    objeto

--- fundamentos/objeto.js
+++ fundamentos/objeto.js
@@ -4,3 +4,10 @@ prod1.preco = 4998.90
 prod1['DescontoLegal'] = 0.40 // evitar atributos com espaço
 
 console.log(prod1)
+
+const prod2 = {
+  nome:'Camisa Polo',
+  preço: 79.90
+}
+
+console.log(prod2)
