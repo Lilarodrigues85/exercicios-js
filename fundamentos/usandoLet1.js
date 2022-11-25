7701470a63839bbf24a093fe456c09a7d53c8fa4
Lila <dalila.analistadesistema@gmail.com>
2022-11-25 17:44:46 -0300

    usandoLet1

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+  let numero = 2
+  console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+/*variáveis definidas com a palavra var, tem escolpo global e escolpo de função
+variáveis definidas com a palavra let, tem escolpo global, escolpo de função e escolpo de bloco*/
