63769db2431d5e709745d3dd0c3a3407b4f0e5f8
Lila <dalila.analistadesistema@gmail.com>
2022-11-29 22:10:28 -0300

    atribuições

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a    // b = b + a junto com a atribuição esta fazendo uma adição
+console.log(b)
+
+b -= 4    // b = b - 4 subtraindo
+console.log(b)
+
+b *= 2 // b = b * 2 multiplicação
+console.log(b)    
+
+b /= 2 // b = b / 2 divisão
+console.log(b)
+
+b %= 2 // b = b % 2 módulo
+console.log(b)
