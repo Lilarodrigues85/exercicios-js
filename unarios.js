095b9d663bd3d00741dbcfad7253922d88515f22
Lila <dalila.analistadesistema@gmail.com>
2022-12-04 00:34:11 -0300

    operadores Unarios

---
+++ unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++ //acrescenta uma unidade na variavel (pós fixado)
+console.log(num1)
+--num1 //subtrai 1 (pré fixado)
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
