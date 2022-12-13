4464d4ad5c5002a913c80a9dd3582b85c4da1150
Lila <dalila.analistadesistema@gmail.com>
2022-12-13 19:27:07 -0300

    while

--- controle/while.js
+++ controle/while.js
@@ -7,4 +7,11 @@ let opcao = 0
 
 while (opcao != -1) {
   opcao = getInteiroAleatorioEntre (-1, 10)
+  console.log(`Opção escolhida foi ${opcao}.`)
 }
+
+console.log('Até a próxima!')
+
+// a estrutura WHILE é uma estrutura de repetição que funciona com verdadeiro ou falso
+// enquanto for verdadeiro ela executa e qdo for falso ela para
+// propria para laços indeterminado de repetições
